df8542d717d0d038633e16cc46f38ebddeb0962e
Wenchun Zhang <wezhang@tibco-support.com>
2016-11-22 14:39:59 -0600

    Generate date format for date only string

--- src/json-schema-generator.js
+++ src/json-schema-generator.js
@@ -43,6 +43,11 @@
 			var regEx = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
 			return regEx.test(value);
 		},
+		isDateOnlyString: function(value){
+			// RFC 3339 Date format '2016-11-22', http://tools.ietf.org/html/rfc3339#section-5.6
+			var regEx = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/;
+			return regEx.test(value);
+		},
 		isEmailString: function(value) {
 			var regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 			return regEx.test(value);
@@ -74,7 +79,9 @@
 			var format = undefined;
 
 			if (typeof value === 'string') {
-				if (this.isDateString(value)) {
+				if (this.isDateOnlyString(value)){
+					format = 'date';
+				} else if (this.isDateString(value)) {
 					format = 'date-time';
 				} else if (this.isEmailString(value)) {
 					format = 'email';
